9b8281d1c508ef2670bf9a861dbc4fd3989e19d1
Julian Zapata <jl.zapata0@gmail.com>
2022-04-06 22:55:57 -0400

    Mobile navbar completed

--- src/routes/components/Navbar.svelte
+++ src/routes/components/Navbar.svelte
@@ -7,18 +7,21 @@
         <button type="button" class="icon-button nav__toggle" id="nav-toggle" on:click="{_ => open = true}">
             <img src="images/icon-menu.svg" alt="">
         </button>
-        <div class="nav__menu" class:nav__menu-open={open} id="nav-menu">
-            <button class="icon-button nav__close" id="nav-close" on:click="{_ => open = false}">
-                <img src="images/icon-close.svg" alt="close">
-            </button>
-            <ul class="nav__list">
-                <li class="nav__item">Collections</li>
-                <li class="nav__item">Men</li>
-                <li class="nav__item">Women</li>
-                <li class="nav__item">About</li>
-                <li class="nav__item">Contact</li>
-            </ul>
+        <div class="nav__menu_container" class:nav__menu_container-open={open} on:click="{_ => open = false}">
+            <div class="nav__menu"  id="nav-menu">
+                <button class="icon-button nav__close" id="nav-close" on:click="{_ => open = false}">
+                    <img src="images/icon-close.svg" alt="close">
+                </button>
+                <ul class="nav__list">
+                    <li class="nav__item"><a class="nav__item-text" href="/">Collections</a></li>
+                    <li class="nav__item"><a class="nav__item-text" href="/">Men</a></li>
+                    <li class="nav__item"><a class="nav__item-text" href="/">Women</a></li>
+                    <li class="nav__item"><a class="nav__item-text" href="/">About</a></li>
+                    <li class="nav__item"><a class="nav__item-text" href="/">Contact</a></li>
+                </ul>
+            </div>
         </div>
+  
         <img src="images/logo.svg" class="nav__logo" alt="logo">
         <div class="nav__user">
             <img src="images/icon-cart.svg" alt="shopping cart">
